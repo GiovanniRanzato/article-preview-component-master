c77239e36a4bda136485b2600abe392699ab1711
Giovanni Ranzato <giovanni.ranzato2010@gmail.com>
2020-09-02 11:39:45 +0200

    Fixed Safari Styles

--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@ let toggleShare = false;
 const data = {
     cover: "./images/drawers.jpg",
     title: "Shift the overall look and feel by adding these wonderful touches to furniture in your home",
-    text: " Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.",
+    text: "Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.",
     author: "Michelle Appleton",
     authorAvatar: "./images/avatar-michelle.jpg",
     date: "28 Jun 2020"
